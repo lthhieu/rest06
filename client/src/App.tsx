63dae5a280bf6ada6831872e0879e42fdb73378d
Hieu ward 3 <lyhieu2024@gmail.com>
2024-10-12 17:00:41 +0700

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline">Hello world</p>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
