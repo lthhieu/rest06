a2ed6c5ca1ec66ce71a9baf5ac45d2921adbcac5
Hieu ward 3 <lyhieu2024@gmail.com>
2024-10-19 17:05:49 +0700

    change import login geader.tsx file

--- client/src/components/public/header.tsx
+++ client/src/components/public/header.tsx
@@ -11,7 +11,7 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
-import Login from './login'
+import { Login } from "@/components/public"
 type subType = {
     title: string,
     slug: string
