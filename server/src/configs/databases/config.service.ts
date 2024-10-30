84d1e771fbfe4cb0f8a43befa037cfe66a07cc38
Hieu ward 3 <lyhieu2024@gmail.com>
2024-10-30 10:53:29 +0700

    use .env file variable in config.service

--- server/src/configs/databases/config.service.ts
+++ server/src/configs/databases/config.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from "@nestjs/common";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+    constructor(private configService: ConfigService) { }
     createTypeOrmOptions(): TypeOrmModuleOptions {
         return {
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'root',
-            database: 'test',
+            host: this.configService.get('DATABASE_HOST') ?? 'localhost',
+            port: this.configService.get('DATABASE_PORT') ?? 5432,
+            username: this.configService.get('DATABASE_USERNAME') ?? 'postgres',
+            password: this.configService.get('DATABASE_PASSWORD') ?? 'root',
+            database: this.configService.get('DATABASE_NAME') ?? 'test',
             entities: ['dist/**/**/*.entity{.ts,.js}'],
             synchronize: true,
         };
