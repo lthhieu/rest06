98dff385696a6c4b30f2832fbcd269da4a68e1d5
Hieu ward 3 <lyhieu2024@gmail.com>
2024-11-06 16:56:17 +0700

    validationPipe

--- server/src/auth/dto/auth-data-google.dto.ts
+++ server/src/auth/dto/auth-data-google.dto.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty } from "class-validator";
+
 export class AuthDataGoogleDto {
+    @IsNotEmpty({ message: 'Tên không được để trống' })
     name: string;
+    @IsNotEmpty({ message: 'Email không được để trống' })
     email: string;
+    @IsNotEmpty({ message: 'Ảnh không được để trống' })
     image: string;
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +13,9 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: ["1", "2"]
   })
+
+  //auto-validation
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
   console.log(`Application is running on: ${port}`);
