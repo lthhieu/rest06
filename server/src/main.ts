578874f118380046c542ed46f135f9da9fde67e2
Hieu ward 3 <lyhieu2024@gmail.com>
2024-11-06 10:46:46 +0700

    versioning

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') ?? 3000
+  //config versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ["1", "2"]
+  })
+
   await app.listen(port);
   console.log(`Application is running on: ${port}`);
 }
