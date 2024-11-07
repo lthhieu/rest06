ffe6552811691d06017d2e438036a20a4f7bfd75
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-11-07 22:14:23 +0700

    comment and rating entity

--- server/src/ratings/entities/rating.entity.ts
+++ server/src/ratings/entities/rating.entity.ts
@@ -1,1 +1,26 @@
-export class Rating {}
+import { Post } from 'src/posts/entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+@Entity()
+export class Rating {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar' })
+    content: string;
+
+    @Column({ type: 'integer', default: 0 })
+    star: number;
+
+    @ManyToOne(() => User, (user) => user.id, {
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    })
+    user: User;
+
+    @ManyToOne(() => Post, (post) => post.id, {
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    })
+    post: Post
+}
