a58893276f440e39394afa67424a0ad49c8a5fd7
Hieu ward 3 <lyhieu2024@gmail.com>
2024-11-08 16:25:52 +0700

    implement protected route and jwt strategy guard

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -4,13 +4,16 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtConfigService } from 'src/configs/jwt/jwt.config.service';
+import { JwtStrategy } from './passports/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 @Module({
   imports: [UsersModule,
+    PassportModule,
     JwtModule.registerAsync({
       useClass: JwtConfigService
     })
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule { }
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -17,8 +18,10 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string, @Request() req) {
+    console.log(req.user)
     return this.usersService.findOne(+id);
   }
 
