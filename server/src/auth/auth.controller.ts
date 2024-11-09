969eb585912de94ba68e088f59fa4a1b831611db
Hieu ward 3 <lyhieu2024@gmail.com>
2024-11-09 10:10:54 +0700

    skip jwt auth guard

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDataGoogleDto } from './dto/auth-data-google.dto';
+import { Public } from 'src/configs/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
+
+    @Public()
     @Post('login-with-google')
     getTokenByGoogle(@Body() data: AuthDataGoogleDto) {
         return this.authService.loginWithGoogle(data);
