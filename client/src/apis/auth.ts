304b90cd33bd583655c97ace439877957bd91140
Hieu ward 3 <lyhieu2024@gmail.com>
2024-11-22 17:04:18 +0700

    interceptor response axios, define datatype for api call

--- client/src/apis/auth.ts
+++ client/src/apis/auth.ts
@@ -1,5 +1,6 @@
 import axios from '@/configs/axios.custom'
+import { CustomResponse } from '@/configs/data.type'
 
 export const apiLoginWithGoogle = (payload: IPayloadGoogle) => {
-    return axios.post('/api/v1/auth/login-with-google', payload)
+    return axios.post<any, CustomResponse>('/api/v1/auth/login-with-google', payload)
 }
---
+++ client/src/configs/data.type.d.ts
@@ -0,0 +1,5 @@
+export interface CustomResponse {
+    message: string,
+    statusCode: number,
+    data: any
+}
